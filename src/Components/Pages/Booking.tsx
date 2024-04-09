59db5702ab104120350134136d2165f1128a4fba
Love Rumar Karlquist <lovebandy@hotmail.se>
2024-04-09 08:06:30 +0200

    Kalender tillagt
    Importerat in npm react calendar
    även standard stylingen

--- src/Components/Pages/Booking.tsx
+++ src/Components/Pages/Booking.tsx
@@ -1,10 +1,14 @@
 import React from "react";
+import Calendar from "../Calendar";
 
 function Booking() {
   return (
-    <div>
-      <h1 style={{ textAlign: "center" }}>Boka tid!</h1>
-    </div>
+    <>
+      <div>
+        <h1 style={{ textAlign: "center" }}>Boka tid!</h1>
+      </div>
+      <Calendar />
+    </>
   );
 }
 
