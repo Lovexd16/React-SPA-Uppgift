ca0764f071e6ea0639c21cbd944c61884f64caa7
Love Rumar Karlquist <lovebandy@hotmail.se>
2024-04-09 10:28:15 +0200

    småfix

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -6,8 +6,8 @@ function NavBar(props: Props) {
   return (
     <div className="navbar">
       <button onClick={() => props.setPage("start")}>Start</button>
-      <button onClick={() => props.setPage("booking")}>Boka Tid</button>
-      <button onClick={() => props.setPage("fooddrinks")}>Mat & Dryck</button>
+      <button onClick={() => props.setPage("booking")}>Boka tid</button>
+      <button onClick={() => props.setPage("fooddrinks")}>Mat & dryck</button>
       <button onClick={() => props.setPage("contact")}>Kontakt</button>
     </div>
   );
